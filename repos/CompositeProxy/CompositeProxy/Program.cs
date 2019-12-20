5ca9edd3dfa0465fb623f1e596a069992b1eb7a0
JK <junkheng@gmail.com>
2019-12-20 16:18:47 +0800

    composite complete

--- repos/CompositeProxy/CompositeProxy/Program.cs
+++ repos/CompositeProxy/CompositeProxy/Program.cs
@@ -6,10 +6,63 @@ using System.Threading.Tasks;
 
 namespace CompositeProxy
 {
+    class Creature
+    {
+        public byte Age;
+        public int X, Y;
+    }
+
+    class Creatures
+    {
+        private readonly int size;
+        private byte[] age;
+        private int[] x, y;
+        public Creatures(int size)
+        {
+            this.size = size;
+            age = new byte[size];
+            x = new int[size];
+            y = new int[size];
+        }
+        public struct CreatureProxy
+        {
+            private readonly Creatures creatures;
+            private readonly int index;
+
+            public CreatureProxy(Creatures creatures, int index)
+            {
+                this.creatures = creatures;
+                this.index = index;
+            }
+
+            public ref byte Age => ref creatures.age[index];
+            public ref int X => ref creatures.x[index];
+            public ref int Y => ref creatures.y[index];
+        }
+
+        public IEnumerator<CreatureProxy> GetEnumerator()
+        {
+            for (int pos=0; pos<size; ++pos)
+                yield return new CreatureProxy(this, pos);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            // Arrays of Structure
+            var creatures = new Creature[100];
+            foreach (var c in creatures)
+            {
+                c.X++;
+            }
+
+            // Structure of Arrays.. Better performance
+            var creatures2 = new Creatures(100);
+            foreach (Creatures.CreatureProxy c in creatures2)
+            {
+                c.X++;
+            }
         }
     }
 }
