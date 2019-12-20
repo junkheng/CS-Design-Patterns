046ed668f81bd2a0c640156895a92ab05466436d
JK <junkheng@gmail.com>
2019-12-20 16:11:41 +0800

    composite proxy

---
+++ repos/CompositeProxy/CompositeProxy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
