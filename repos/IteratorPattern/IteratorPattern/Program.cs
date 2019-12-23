9279b1c72eed89826d26ef2fa7caefab29cf14d7
JK <junkheng@gmail.com>
2019-12-23 18:15:24 +0800

    iterator pattern 1

---
+++ repos/IteratorPattern/IteratorPattern/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+
+            left.Parent = right.Parent = this;
+        }
+    }
+
+    public class InOrderIterator<T>
+    {
+        private readonly Node<T> root;
+        public Node<T> Current;
+        private bool yieldedStart;
+
+        public InOrderIterator(Node<T> root)
+        {
+            this.root = root;
+            Current = root;
+            while (Current.Left != null)
+                Current = Current.Left;
+        }
+
+        public bool MoveNext()
+        {
+            if (!yieldedStart)
+            {
+                yieldedStart = true;
+                return true;
+            }
+
+            if (Current.Right != null)
+            {
+                Current = Current.Right;
+                while (Current.Left != null)
+                    Current = Current.Left;
+                return true;
+            }
+            else
+            {
+                var p = Current.Parent;
+                while (p != null && Current == p.Right)
+                {
+                    Current = p;
+                    p = p.Parent;
+                }
+
+                Current = p;
+                return Current != null;
+            }
+        }
+
+        public void Reset()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   1
+            //  / \
+            // 2   3
+
+            // in-order: 213
+            // preorder: 123
+
+            var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
+
+            var it = new InOrderIterator<int>(root);
+            while (it.MoveNext())
+            {
+                Console.Write(it.Current.Value);
+                Console.Write(',');
+            }
+            Console.WriteLine();
+        }
+    }
+}
