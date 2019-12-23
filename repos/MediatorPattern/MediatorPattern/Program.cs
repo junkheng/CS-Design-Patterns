5f1188c805860f84960a281f24e82061a177892e
JK <junkheng@gmail.com>
2019-12-23 19:37:32 +0800

    mediator pattern

---
+++ repos/MediatorPattern/MediatorPattern/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+    public class Person
+    {
+        public string Name;
+        public ChatRoom Room;
+        private List<string> chatlog = new List<string>();
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void Say(string message)
+        {
+            Room.Broadcast(Name, message);
+        }
+
+        public void PrivateMessage(string who, string message)
+        {
+            Room.Message(Name, who, message);
+        }
+
+        public void Receive(string sender, string message)
+        {
+            string s = $"{sender}: '{message}'";
+            chatlog.Add(s);
+            Console.WriteLine($"[{Name}'s chat session] {s}");
+        }
+    }
+
+    public class ChatRoom
+    {
+        private List<Person> people = new List<Person>();
+
+        public void Join(Person p)
+        {
+            string joinMsg = $"{p.Name} joined the chat";
+            Broadcast("room", joinMsg);
+
+            p.Room = this;
+            people.Add(p);
+        }
+
+        public void Broadcast(string source, string message)
+        {
+            foreach (var p in people)
+                if (p.Name != source)
+                    p.Receive(source, message);
+        }
+
+        public void Message(string source, string destination, string message)
+        {
+            people.FirstOrDefault(p => p.Name == destination)
+                ?.Receive(source, message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var room = new ChatRoom();
+            var john = new Person("John");
+            var jane = new Person("Jane");
+
+            room.Join(john);
+            room.Join(jane);
+
+            john.Say("hi");
+            jane.Say("hey John!");
+
+            var simon = new Person("Simon");
+            room.Join(simon);
+            simon.Say("hi everyone!");
+
+            jane.PrivateMessage("Simon", "glad you could join us!");
+        }
+    }
+}
