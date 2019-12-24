5332bb4597823c51942608eb007f9c8df90be7ac
JK <junkheng@gmail.com>
2019-12-24 17:02:35 +0800

    strategy pattern dynamic

---
+++ repos/StrategyDynamicPattern/StrategyDynamicPattern/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace StrategyDynamicPattern
+{
+    public enum OutputFormat
+    {
+        Markdown,
+        Html
+    }
+
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+        void End(StringBuilder sb);
+        void AddListItems(StringBuilder sb, string item);
+    }
+
+    public class HtmlListStrategy : IListStrategy
+    {
+        public void Start(StringBuilder sb)
+        {
+            sb.AppendLine("<ul>");
+        }
+
+        public void End(StringBuilder sb)
+        {
+            sb.AppendLine("</ul>");
+        }
+
+        public void AddListItems(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"    <li>{item}</li>");
+        }
+    }
+
+    public class MarkdownListStrategy : IListStrategy
+    {
+        public void Start(StringBuilder sb)
+        {
+            
+        }
+
+        public void End(StringBuilder sb)
+        {
+            
+        }
+
+        public void AddListItems(StringBuilder sb, string item)
+        {
+            sb.AppendLine($" * {item}");
+        }
+    }
+
+    public class TextProcessor
+    {
+        private StringBuilder sb = new StringBuilder();
+        private IListStrategy listStrategy;
+
+        public void SetOutputFormat(OutputFormat format)
+        {
+            switch (format)
+            {
+                case OutputFormat.Markdown:
+                    listStrategy = new MarkdownListStrategy();
+                    break;
+                case OutputFormat.Html:
+                    listStrategy = new HtmlListStrategy();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(format), format, null);
+            }
+        }
+
+        public void AppendList(IEnumerable<string> items)
+        {
+            listStrategy.Start(sb);
+            foreach (var item in items)
+                listStrategy.AddListItems(sb, item);
+            listStrategy.End(sb);
+        }
+
+        public StringBuilder Clear()
+        {
+            return sb.Clear();
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tp = new TextProcessor();
+            tp.SetOutputFormat(OutputFormat.Markdown);
+            tp.AppendList(new [] {"foo", "bar", "baz"});
+            WriteLine(tp);
+
+            tp.Clear();
+            tp.SetOutputFormat(OutputFormat.Html);
+            tp.AppendList(new [] {"foo", "bar", "baz"});
+            WriteLine(tp);
+        }
+    }
+}
