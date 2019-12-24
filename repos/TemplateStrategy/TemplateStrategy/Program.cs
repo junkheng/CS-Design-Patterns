cc0c9965209ac4e4470ae44fb8f968f442d106ca
JK <junkheng@gmail.com>
2019-12-24 17:37:26 +0800

    template pattern

---
+++ repos/TemplateStrategy/TemplateStrategy/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TemplateStrategy
+{
+    public abstract class Game
+    {
+        public void Run()
+        {
+            Start();
+            while (!HaveWinner) 
+                TakeTurn();
+            WriteLine($"Player {WinningPlayer} wins.");
+        }
+
+        protected int currentPlayer;
+        protected readonly int numberOfPlayers;
+
+        protected Game(int numberOfPlayers)
+        {
+            this.numberOfPlayers = numberOfPlayers;
+        }
+        protected abstract void Start();
+        protected abstract void TakeTurn();
+        protected abstract bool HaveWinner { get; }
+        protected abstract int WinningPlayer { get; }
+
+    }
+
+    public class Chess : Game
+    {
+        public Chess() : base(2)
+        {
+        }
+
+        protected override void Start()
+        {
+            WriteLine($"Starting a game of chess with {numberOfPlayers} players.");
+        }
+
+        protected override void TakeTurn()
+        {
+            WriteLine($"Turn {turn++} taken by player {currentPlayer}.");
+            currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+        }
+
+        protected override bool HaveWinner => turn == maxTurns;
+        protected override int WinningPlayer => currentPlayer;
+        private int turn = 1;
+        private int maxTurns = 10;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chess = new Chess();
+            chess.Run();
+        }
+    }
+}
